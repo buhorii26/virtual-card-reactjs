06cfc52c3148137873676f8da1c8b406bdd276e8
Buhori <buhorics@gmail.com>
2023-12-22 13:37:18 +0900

    first commit

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const virtualCard = [
+    {
+        nama: 'Buhori',
+        profesi: 'Web Developer',
+        about: 'Saya adalah seorang programmer',
+        img:'assets/bori.JPG',
+    }
+]
+
+export default virtualCard;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,110 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import data from "./data";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const socialMedia = [
+  {
+    icon: "fa-facebook",
+    link: "https://www.facebook.com/buhorii26",
+  },
+  {
+    icon: "fa-instagram",
+    link: "https://www.instagram.com/buhorii26/",
+  },
+  {
+    icon: "fa-linkedin",
+    link: "https://www.linkedin.com/in/buhori-067235126",
+  },
+  {
+    icon: "fa-github",
+    link: "https://github.com/buhorii26",
+  },
+];
+function App() {
+  return (
+    <body>
+      <div className="card">
+        <div className="top">
+          <Header />
+          <Indentitas />
+        </div>
+        <div className="bottom">
+          <Biodata />
+          <Highlight />
+        </div>
+      </div>
+    </body>
+  );
+}
+
+function SocialButton({ icon, link }) {
+  return (
+    <button onClick={() => window.open(link, "_blank")}>
+      <i className={`fa ${icon}`}></i>
+    </button>
+  );
+}
+function Header() {
+  const middleIndex = Math.floor(socialMedia.length / 2);
+  const socialLeft = socialMedia.slice(0, middleIndex);
+  const socialRight = socialMedia.slice(middleIndex);
+
+  return (
+    <>
+      <div className="social-buttons">
+        {socialLeft.map((data, index) => (
+          <SocialButton icon={data.icon} link={data.link} />
+        ))}
+      </div>
+      <div className="social-buttons right">
+      {socialRight.map((data, index) => (
+          <SocialButton icon={data.icon} link={data.link} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function Indentitas() {
+  return (
+    <div className="text">
+      <div className="name-wrapper">
+        <div className="name">Buhori</div>
+      </div>
+      <div className="title">Web Developer</div>
+    </div>
+  );
+}
+function Biodata() {
+  return (
+    <div className="desc">
+      <p>
+        My name is <b>Buhori</b> & Web Developer
+      </p>
+    </div>
+  );
+}
+
+function Badge({ text }) {
+  return (
+    <button>
+      <div className="height">{text}</div>
+    </button>
+  );
+}
+function Highlight() {
+  return (
+    <div className="buttons">
+      <Badge text="Front End" />
+      <Badge text="Back End" />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
